8088c30254823e18326023ffa2af1f6e01695bd7
Lorenzo Barretta <43916566+DevLBD@users.noreply.github.com>
2022-05-08 10:58:17 +0200

    Worked on app theme.
    Working on the color scheme. Will continue later.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:kolibri/ui/home.dart';
 
+// Color Scheme.
 const color01 = Color(0xFFDE00FF);
+const color02 = Color(0xFFF8A6CD);
 
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
+      // Theming the app bar.
+      appBarTheme: const AppBarTheme(
+        foregroundColor: Colors.black,
+      ),
+      // Theming the rest of the app.
       colorScheme: ColorScheme.fromSwatch().copyWith(primary: color01),
-      brightness: Brightness.dark,
+      scaffoldBackgroundColor: color02,
     ),
+    // Setting the app title and telling Flutter what's the Home class.
     title: "Kolibri App",
     home: const Home(),
   ));
--- app/lib/ui/home.dart
+++ app/lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kolibri/main.dart';
 
 class Home extends StatefulWidget {
@@ -11,12 +12,20 @@ class Home extends StatefulWidget {
 class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    // Setting status and navigation bar colors.
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: color01,
+      systemNavigationBarColor: color02,
+    ));
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Patrick Scem"),
-        ),
-        body: ListView(
-          children: const [Center(child: Text("Patrick Scem"))],
-        ));
+      appBar: AppBar(
+          title: const Text("Kolibri App"),
+          centerTitle: true,
+          elevation: 0,
+          shape:
+              const Border(bottom: BorderSide(width: 2, color: Colors.black))),
+      body: const Center(child: Text("Hello, Kolibri!")),
+    );
   }
 }
