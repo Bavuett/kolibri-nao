2c4d00dd1854699560519d9d9bbbbc2a570a562d
Lorenzo Barretta <43916566+DevLBD@users.noreply.github.com>
2022-05-08 10:04:25 +0200

    Playing around with colors.
    Beginning to theme the app to pass some time. Will work further on the app later.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,7 +5,10 @@ const color01 = Color(0xFFDE00FF);
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(primaryColor: color01),
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSwatch().copyWith(primary: color01),
+      brightness: Brightness.dark,
+    ),
     title: "Kolibri App",
     home: const Home(),
   ));
