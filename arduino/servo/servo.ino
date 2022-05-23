2fdafbdc5451c0368413433f36f49babda2231ff
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2022-05-23 11:08:35 +0200

    Testing Arduino Sketch - successful.

--- arduino/servo/servo.ino
+++ arduino/servo/servo.ino
@@ -5,13 +5,8 @@ Servo m1;
 void setup() {
     // Setup code to run once.
     m1.attach(5);
-    m1.write(180);
-    delay(2000);
-    m1.write(0);
-    delay(2000);
-    m1.write(20);
-    delay(2000);
-    m1.write(0);
+    m1.writeMicroseconds(5000);
+    delay(500);
 }
 
 void loop() {
