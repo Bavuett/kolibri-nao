19ea9a9fa5b5c6cc6d6463ef0dba7508f91a1e8a
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2022-05-27 15:32:09 +0200

    Working on stopping Socket.
    Now the Socket Server is able to close itself.

--- simple-server/socket.py
+++ simple-server/socket.py
@@ -4,7 +4,7 @@ __author__ = 'uva'
 '''
 Synchronous tcp server
 '''
-
+active = True
 # For tcp
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # To fix the address already in use issue
@@ -14,14 +14,23 @@ sock.bind(("0.0.0.0", 8080))
 # Number of backlog clients
 sock.listen(2)
 print("Waiting for client....!")
-while True:
+while active == True:
     (client, (ip, port)) = sock.accept()
     print('client connected with ip as {} and port {}'.format(ip, port))
     data = client.recv(2048)
+
     while len(data):
-        print(len(data))
         print("Client sent the data : {}".format(data))
         client.send(data.upper())
+
+        received = data.decode("utf8")
+
+        if received == "":
+            print(received)
+        elif received == "close":
+            active = False
+            print("Closing the Socket.")
+        
         data = client.recv(2048)
     print("Client closed connection !!! :( ")
     client.close()
