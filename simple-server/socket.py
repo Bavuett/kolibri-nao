65633820347f99a24f71030afca3513d3591e6da
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2022-05-29 19:02:06 +0200

    Updating test socket.

--- simple-server/socket.py
+++ simple-server/socket.py
@@ -1,9 +1,9 @@
 import socket
 
-__author__ = 'uva'
-'''
-Synchronous tcp server
-'''
+def generateBytes(value):
+    ToSend = bytes(value, "utf-8")
+    return ToSend
+
 active = True
 # For tcp
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,28 +13,34 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(("0.0.0.0", 8080))
 # Number of backlog clients
 sock.listen(2)
-print("Waiting for client....!")
+print("Welcome! Waiting for Client...")
 while active == True:
     (client, (ip, port)) = sock.accept()
-    print('client connected with ip as {} and port {}'.format(ip, port))
+    print('\nClient connected with IP as {} and Port {}'.format(ip, port))
     data = client.recv(2048)
 
     while len(data):
-        print("Client sent the data : {}".format(data))
-        client.send(data.upper())
+        received = data.lower().decode("utf8")
 
-        received = data.decode("utf8")
+        print("Client sent '{}'".format(str(received)))
 
         if received == "":
             print(received)
         elif received == "close":
             active = False
-            print("Closing the Socket.")
+            print("\nI'll close the Socket when the Client closes his connection.")
+        elif received == "mostrami":
+            client.send(generateBytes("showtime"))
+
+            message = input(str("Send a custom message: "))
+            client.send(generateBytes(message))
         
         data = client.recv(2048)
-    print("Client closed connection !!! :( ")
+    print("\nClient closed the connection.")
     client.close()
 
 
-print("Closing the Socket!!")
+print("Closing the Socket.")
 sock.close()
+
+
